44a9ab4e78d3192bb06d88b5f8adfe57148b79e5
ououio <reona.ou@gmail.com>
2024-04-29 08:06:33 +0800

    根据以上git diff信息生成一行commit message：
    更新了mapper文件，添加了新的查询语句。
    
    请根据以上git diff信息生成一行commit message，二十个字以内。

--- IDEA/Kotlin1/src/Main.kt
+++ IDEA/Kotlin1/src/Main.kt
@@ -1,17 +1,58 @@
+import java.text.DecimalFormat
+import java.util.Locale
+import java.util.Locale.*
+
+
 fun main()
 {
-    var a: Array<Int> = arrayOf(5, 6, 7, 8, 9, 0)
-    var b: Array<String> = arrayOf("aaa", "bbb", "ccc", "ddd", "eee", "fff")
-    var c: Array<Boolean> = arrayOf(true, false, true, false, true, false)
-    var d: Array<Double> = arrayOf(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
-    traverseArray(a)
-    traverseArray(b)
-    traverseArray(c)
-    traverseArray(d)
+    val sorigin: String = "abc.def.ghi.jkl,opq,201410598210"
+    var origin = sorigin
+    println(origin)
+    if (origin.indexOf('.') >= 0)
+    {
+        origin.substring(0, origin.indexOf('.'))
+    }
+
+    var corigin: CharArray = stringToCharArray(origin)
+    println(corigin)
+
+    var forigin = origin.replace(',', ';')
+    println(forigin)
+
+    var result0 = origin.split(',')
+    for (result in result0)
+    {
+        println(result)
+    }
+
+    var five = origin.get(4)
+    println(five)
+
+    var len = origin.length
+    println(len)
+
+    println(dollar(origin))
 }
 
-fun <T> traverseArray(array: Array<T>)
+fun stringToCharArray(s: String): CharArray
 {
-    var o = array.joinToString("~")
-    println(o)
+    val charArray = s.toCharArray()
+    val result = mutableListOf<Char>()
+    for (i in charArray.indices)
+    {
+        result.add(charArray[i])
+        if (i < charArray.size - 1)
+        {
+            result.add('-')
+        }
+    }
+
+    return result.toCharArray()
+}
+
+fun dollar(s: String): String
+{
+
+    var n= Regex("\\d").findAll(s).toString()
+    return n
 }
